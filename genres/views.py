96d70495fe466cc76ee0cf6b8b17102cffbf9682
Arthur Abreu <arthuraudayadreu@gmail.com>
2026-04-01 20:11:28 -0300

    Primeiro commit da API

---
+++ genres/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from genres.models import Genre
+from .serializers import GenreSerializer    
+
+
+class GenreCreateListView(generics. ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class GenreDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
