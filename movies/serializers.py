ac56b6a20bab38ca7a9a3a09bdb5c43d539bbe66
Arthur Abreu <arthuraudayadreu@gmail.com>
2026-05-06 20:42:29 -0300

    Updating the serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,3 +5,17 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self,value):
+        if value.year < 1990:
+            raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("O resumo não pode exceder 200 caracteres.")
+        elif len(value) < 10:
+            raise serializers.ValidationError("O resumo deve conter pelo menos 10 caracteres.")
+        return value
+        
+
--- reviews/serializers.py
+++ reviews/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Review
         fields = '__all__'
